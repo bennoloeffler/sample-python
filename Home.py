6c3183006351397361d6153cd57d4c23596abc29
Armin Rothfuss <armin@rothfuss-web.de>
2024-05-03 06:53:59 +0200

    Add sub-pages

---
+++ Home.py
@@ -0,0 +1,17 @@
+import streamlit as st
+
+st.set_page_config(
+    page_title="V&S AI Apps",
+    layout='wide'
+)
+
+st.header("V&S AI applications")
+st.write("""
+[![view source code ](https://img.shields.io/badge/GitHub%20Repository-gray?logo=github)](https://github.com/bennoloeffler/sample-python)
+""")
+st.write("""
+Here you find the V&S AI Applications
+
+- **Chat with the bot**: Engage in interactive conversations with the LLM.
+
+""")
