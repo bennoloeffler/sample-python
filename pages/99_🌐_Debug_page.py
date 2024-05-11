c67632428ea08cf572e58e1844db5a6a3c31ee30
Armin Rothfuss <armin@rothfuss-web.de>
2024-05-11 10:53:23 +0200

    Protect with password Filter out _KEYs

--- pages/99_🌐_Debug_page.py
+++ pages/99_🌐_Debug_page.py
@@ -2,17 +2,25 @@ import os
 import streamlit as st
 from streamlit_extras.app_logo import add_logo
 
+from password import check_password
+
+
 st.set_page_config(page_title="Debug page", layout="wide", page_icon='🌐')
 with st.sidebar:
     add_logo("img/v-und-s.png")
 
 st.title("Debug page")
+
+if not check_password():
+    st.stop()
+
+
 st.subheader("Session Data:")
 ses = list(map(lambda i: {"key": i[0], "value": i[1]}, st.session_state.to_dict().items()))
 st.dataframe(ses, use_container_width=True)
 
 st.subheader("Environment:")
-env = list(map(lambda i: {"key": i, "value": os.getenv(i)}, sorted(os.environ)))
+env = list(map(lambda i: {"key": i, "value": os.getenv(i)}, filter(lambda i: not i.endswith("_KEY"), sorted(os.environ))))
 st.dataframe(env, use_container_width=True)
 
 st.subheader("Config Data:")
