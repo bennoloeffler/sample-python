f6b3f34700849cbbaa961583b6a24fd52e6873e9
Armin Rothfuss <armin@rothfuss-web.de>
2024-05-11 06:46:29 +0200

    Display enironment on debug page Display data in dataframes on debug page

--- pages/99_🌐_Debug_page.py
+++ pages/99_🌐_Debug_page.py
@@ -1,3 +1,4 @@
+import os
 import streamlit as st
 from streamlit_extras.app_logo import add_logo
 
@@ -6,11 +7,18 @@ with st.sidebar:
     add_logo("img/v-und-s.png")
 
 st.title("Debug page")
-st.subheader("Session Data")
-st.write(st.session_state)
-st.subheader("Config Data")
+st.subheader("Session Data:")
+ses = list(map(lambda i: {"key": i[0], "value": i[1]}, st.session_state.to_dict().items()))
+st.dataframe(ses, use_container_width=True)
 
-config = []
-for c in st.config.get_config_options().values():
-    config.append(f'{c.key}: "{c.value}" is default: {c.is_default})')
-st.write(config)
+st.subheader("Environment:")
+env = list(map(lambda i: {"key": i, "value": os.getenv(i)}, sorted(os.environ)))
+st.dataframe(env, use_container_width=True)
+
+st.subheader("Config Data:")
+conf = list(map(lambda i: {
+    "key": i.key,
+    "value": i.value,
+    "default": i.is_default},
+    st.config.get_config_options().values()))
+st.dataframe(conf, use_container_width=True)
